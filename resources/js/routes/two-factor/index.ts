cd9b534b2494996d08e921446c086556214c250f
Lyhour <kouchlyhour@gmail.com>
2026-04-27 09:03:35 +0700

    style: update NIB color palette and clean up unused two-factor authentication route definitions

--- resources/js/routes/two-factor/index.ts
+++ resources/js/routes/two-factor/index.ts
@@ -45,15 +45,6 @@ login.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     method: 'head',
 })
 
-/**
-* @see \Laravel\Fortify\Http\Controllers\TwoFactorAuthenticatedSessionController::login
-* @see vendor/laravel/fortify/src/Http/Controllers/TwoFactorAuthenticatedSessionController.php:41
-* @route '/two-factor-challenge'
-*/
-const loginForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-    action: login.url(options),
-    method: 'get',
-})
 
 /**
 * @see \Laravel\Fortify\Http\Controllers\TwoFactorAuthenticatedSessionController::login
@@ -82,15 +73,6 @@ loginForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
 
 login.form = loginForm
 
-/**
-* @see \Laravel\Fortify\Http\Controllers\TwoFactorAuthenticatedSessionController::login
-* @see vendor/laravel/fortify/src/Http/Controllers/TwoFactorAuthenticatedSessionController.php:56
-* @route '/two-factor-challenge'
-*/
-export const login = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
-    url: login.url(options),
-    method: 'post',
-})
 
 login.definition = {
     methods: ["post"],
